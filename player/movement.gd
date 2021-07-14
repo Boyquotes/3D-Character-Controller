70a8b22b5ab9a29f2c17badac9a49c40000ee672
sawendev <77589154+sawendev@users.noreply.github.com>
2021-07-14 18:33:09 -0500

    Disabled air movement and enabled full screen

--- player/movement.gd
+++ player/movement.gd
@@ -11,20 +11,21 @@ var acceleration = 10.0
 
 func _physics_process(delta):
 	
-	var target_velocity = Vector3.ZERO
-	if Input.is_action_pressed("walk_up"):
-		target_velocity -= $HRot.transform.basis.z
-	if Input.is_action_pressed("walk_down"):
-		target_velocity += $HRot.transform.basis.z
-	if Input.is_action_pressed("walk_left"):
-		target_velocity -= $HRot.transform.basis.x
-	if Input.is_action_pressed("walk_right"):
-		target_velocity += $HRot.transform.basis.x
-	if target_velocity != Vector3.ZERO:
-		target_velocity = target_velocity.normalized() * walk_speed
-	hvel = lerp(hvel, target_velocity, acceleration * delta)
-	
 	if is_on_floor():
+		
+		var target_velocity = Vector3.ZERO
+		if Input.is_action_pressed("walk_up"):
+			target_velocity -= $HRot.transform.basis.z
+		if Input.is_action_pressed("walk_down"):
+			target_velocity += $HRot.transform.basis.z
+		if Input.is_action_pressed("walk_left"):
+			target_velocity -= $HRot.transform.basis.x
+		if Input.is_action_pressed("walk_right"):
+			target_velocity += $HRot.transform.basis.x
+		if target_velocity != Vector3.ZERO:
+			target_velocity = target_velocity.normalized() * walk_speed
+		hvel = lerp(hvel, target_velocity, acceleration * delta)
+		
 		if Input.is_action_pressed("jump"):
 			vvel.y = jump_force
 		else:
